b7a359ab4e7de1cbf00813c43e86279f6b44b6fc
envy_life <f20200068@pilani.bits-pilani.ac.in>
2022-02-06 19:27:32 +0530

    Fixed and removed some logs

--- test/ERC20CRV.js
+++ test/ERC20CRV.js
@@ -15,37 +15,37 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
     describe('deployment checks for ERC20CRV', () => {
 
         it('Should deploy the smart contract properly', async() => {
-            console.log(token.address);
+
             assert(token.address !== '');
         })
 
         it('Check the name of the token', async() => {
             const result = await token.name();
-            console.log(result);
+
             assert(result === "CAPX Token");
         })
 
         it('Check the symbol of the token', async() => {
             const result = await token.symbol();
-            console.log(result);
+
             assert(result === "CAPX");
         })
 
         it('Check the decimals', async() => {
             const result = await token.decimals();
-            console.log(result.toNumber());
+
             assert(result.toNumber() === 18);
         })
 
         it('Check the total supply', async() => {
             const result = await token.totalSupply();
-            console.log(result.toNumber());
+
             assert(result.toNumber() === 1303030303);
         })
 
         it('Check the balance of any user', async() => {
             const result = await token.balanceOf(deployer);
-            console.log("The balance of the given user is: ", result.toString());
+
             assert(result.toString() === "1303030303");
         })
 
@@ -57,7 +57,7 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
         it('Checking current supply at and minting', async() => {
             let result;
             result = await token.totalSupplyAt(0);
-            console.log("The total Supply at the block number 0 is", result.toNumber());
+
             result = await token.mint(mintreceiver, 100);
             result = await token.totalSupplyAt(0)
             const result1 = await token.totalSupply()
@@ -71,7 +71,7 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
             // allowance() function
             await token.approve(receiver, 20 ,{from : deployer});
             const result = await token.allowance(deployer, receiver);
-            console.log("The amount that the owner is allowed to a spender is", result.toString());
+
             assert(result.toNumber() == 20, "Function is not working correctly");
         })
 
@@ -84,12 +84,12 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
 
             // before transfer
             balanceOf = await token.balanceOf(deployer);
-            console.log("deployer balance before transfer", balanceOf.toString());
+
             balanceOf = await token.balanceOf(receiver);
-            console.log("receiver balance before transfer", balanceOf.toString());
+
 
             const value = await token.allowance(deployer, receiver);
-            console.log("The value is :", value.toString());
+
 
             await token.approve(receiver, 100, { from: deployer });
             // transfer
@@ -97,9 +97,9 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
 
             // after transfer
             balanceOf = await token.balanceOf(deployer);
-            console.log("deployer balance after transfer", balanceOf.toString());
+
             balanceOf = await token.balanceOf(receiver);
-            console.log("receiver balance after transfer", balanceOf.toString());
+
         })
 
 
@@ -108,9 +108,9 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
 
             // before transferFrom()
             let balance1 = await token.balanceOf(deployer);
-            console.log("deployer balance before from transfer", balance1.toString())
+
             let balance2 = await token.balanceOf(receiver);
-            console.log("receiver balance before transfer", balance2.toString())
+
 
             await token.approve(sender, 100, {from : deployer});
             let value_to_send = 20;
@@ -119,9 +119,9 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
 
             // after transferFrom()
             balance3 = await token.balanceOf(deployer);
-            console.log("deployer balance after transfer", balance3.toString());
+
             balance4 = await token.balanceOf(receiver);
-            console.log("receiver balance after transfer", balance4.toString());
+
 
             assert(balance1.toNumber() - balance3.toNumber() == value_to_send && balance4.toNumber() - balance2.toNumber() == value_to_send , "Did not pass the test case");
         })
@@ -134,13 +134,13 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
             const balance1 = await token.balanceOf(sender);
             const balance2 = await token.balanceOf(deployer);
 
-            console.log("Deployer amount before approve: ", balance2.toString());
+
             const result = await token.approve(spender_address, value, {from : deployer});
             const balance3 = await token.balanceOf(sender);
             const balance4 = await token.balanceOf(deployer);
-            console.log("Deployer amount after approve: ", balance4.toString());
 
-            console.log("Allowed:", result);
+
+
         })
 
 
@@ -153,10 +153,10 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
             // burn() function
             let currentTotalSupply
             currentTotalSupply = await token.totalSupply();
-            console.log("Before burning, the total supply is:", currentTotalSupply.toString());
+
             const result = await token.burn(3);
             const newCurrentTotalSupply = await token.totalSupply();
-            console.log("After burning, the totla supply is:", newCurrentTotalSupply.toString());
+
 
             assert(newCurrentTotalSupply <= currentTotalSupply);
         })
@@ -168,14 +168,14 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
 
             // getting the admin address: 
             const admin = await token.get_admin();
-            console.log("The current admin is: ", admin);
+
 
             assert(admin.toString() === deployer.toString(), "You are not the admin");
             await token.set_admin(sender);
-            console.log("The address to put as new admin is", sender);
+
 
             let new_admin = await token.get_admin();
-            console.log("The new admin is:", new_admin);
+
 
             assert(new_admin.toString() === sender.toString(), "Function not working correctly");
         })
@@ -188,15 +188,15 @@ contract ('ERC20CRV', ([deployer, receiver, sender, mintreceiver]) => {
             const symbol = "HELL"
 
             const current_admin = await token.get_admin();
-            console.log("The current admin is: ", current_admin)
+
 
             await token.set_name(token_name, symbol);
 
             let new_token = await token.name();
             let new_symbol = await token.symbol();
 
-            console.log("The new token name is: ", new_token);
-            console.log("The new symbol name is: ", new_symbol);
+
+
 
             assert(new_token.toString() === token_name.toString());
             assert(new_symbol.toString() === symbol.toString());
--- test/VotingEscrow.js
+++ test/VotingEscrow.js
@@ -33,16 +33,16 @@ contract ('VotingEscrow', ([deployer, receiver, sender, checker, testUser]) => {
 
             const future_admin = sender;
             let current_admin = await escrow.get_admin();
-            console.log("The current admin before anything ", current_admin);
+            
             await escrow.commit_transfer_ownership(future_admin, {from : deployer});
 
             current_admin = await escrow.get_admin();
-            console.log("The admin after commiting is still: ", current_admin);
+            
 
             await escrow.apply_transfer_ownership({from: deployer});
             current_admin = await escrow.get_admin();
 
-            console.log("The new admin after apply ", current_admin);
+            
             assert(current_admin.toString() === future_admin, "Function not working");
         })
     })    
@@ -53,16 +53,16 @@ contract ('VotingEscrow', ([deployer, receiver, sender, checker, testUser]) => {
             
             const smart_wallet_checker_address = checker;
             let current_checker = await escrow.get_smart_wallet_checker();
-            console.log("The current checker before anything ", current_checker);
+            
             await escrow.commit_smart_wallet_checker(smart_wallet_checker_address, {from : deployer});
 
             current_checker = await escrow.get_smart_wallet_checker();
-            console.log("The checker after commit is still: ", current_checker);
+            
 
             await escrow.apply_smart_wallet_checker({from: deployer});
             current_checker = await escrow.get_smart_wallet_checker();
 
-            console.log("The new checker after apply is: ", current_checker);
+            
             assert(current_checker.toString() === smart_wallet_checker_address.toString(), "Function not working");
         })
     })
@@ -74,7 +74,7 @@ contract ('VotingEscrow', ([deployer, receiver, sender, checker, testUser]) => {
             
             // before creating the lock
             balance = await escrow.balanceOf(address);
-            console.log("The balance of the user before creating the lock: ", balance.toNumber());
+            
 
             // creating a lock
             await token.approve(escrow.address , value);
@@ -82,7 +82,7 @@ contract ('VotingEscrow', ([deployer, receiver, sender, checker, testUser]) => {
 
             // after creating the lock
             const balance1 = await escrow.balanceOf(address);
-            console.log("Balance of deployer after creating lock: ", balance1.toNumber());
+            
             assert(balance.toNumber() != balance1.toNumber(), "Function not working");
         })
     })
@@ -99,21 +99,21 @@ contract ('VotingEscrow', ([deployer, receiver, sender, checker, testUser]) => {
         // throwing gas consumption error
         it('increase the amount to be locked', async() => {
             balance = await escrow.balanceOf(address);
-            console.log("The balance of the user before increase amount is: ", balance.toNumber());
+            
             value = 126144001;
             await escrow.increase_amount(value);
             balance1 = await escrow.balanceOf(address);
-            console.log("The balance of the user is: ", balance.toNumber());
+            
             assert(balance1.toNumber() != balance.toNumber(), "Function not working")
         })
 
         it('increase the unlock time', async() => {
             const current_end = await escrow.locked__end(address);
-            console.log("Unlock time before increasing unlock:", current_end.toNumber());
+            
             unlock_time = 1676242000;
             await escrow.increase_unlock_time(unlock_time);
             const next_end = await escrow.locked__end(address);
-            console.log("Unlock time after increasing unlock:", next_end.toNumber());
+            
         })            
 
         it('gets the voting power at an epoch', async() => {
@@ -122,20 +122,20 @@ contract ('VotingEscrow', ([deployer, receiver, sender, checker, testUser]) => {
             epoch_time = epoch_time.getTime() / 1000;
             const addr = deployer;
             result = await escrow.getVotingPowerAt(addr, unlock_time);
-            console.log("The user voting power is:", result.toString());
+            
         })
         
         it('gets the voting power of the msg.sender', async() => {
             let voting_power = await escrow.balanceOf(address,unlock_time, {from: deployer});
-            console.log("The voting power of the address is", voting_power.toString());
+            
         })
 
         it('gets the voting power at a block height', async() => {
             let block_number = await escrow.get_block_number();
-            console.log("Current block number", block_number.toString())
+            
             const block_height = block_number.toNumber();
             result = await escrow.balanceOfAt(address, block_height);
-            console.log("The balance of the user at block "+ block_number + " is: ", result.toString());
+            
             assert(parseInt(result.toString()) != 0, "Function not working");
         })
         
@@ -157,16 +157,16 @@ contract ('VotingEscrow', ([deployer, receiver, sender, checker, testUser]) => {
         it('calculates the total voting power at an epoch time', async() => {
             epoch_time = 1646241122
             result = await escrow.totalSupply(epoch_time);
-            console.log("The total voting power is: ", result.toString());
+            
             assert.isNumber(result); 
         })
         
         it('calculates the total voting power at a block in the past', async() => {
             let block_number = await escrow.get_block_number();
-            console.log("Current block number", block_number.toString())
+            
             const block_height = block_number.toNumber();
             result = await escrow.totalSupplyAt(block_height);
-            console.log("The total voting power at the point is: ", result.toString())
+            
             assert(parseInt(result.toString()) != 0, "Function not working");
         })
     })
